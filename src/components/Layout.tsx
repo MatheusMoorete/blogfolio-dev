3e0947a2d59b840faf4c0c5d8f1295e34d8b2a2a
MatheusMoorete <matheus.moorete@gmail.com>
2026-01-14 19:44:30 -0300

    correção do bg

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -21,7 +21,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
             width: '100%'
         }}>
             <Navbar />
-            <main style={{ width: '100%', overflow: 'hidden' }}>
+            <main style={{ width: '100%' }}>
                 {children}
             </main>
             <footer style={{
